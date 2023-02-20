d1c47565a0d3956eb611dc506217516aca7d48d4
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-02-20 13:09:20 +0200

    update

--- 01 First Steps in OOP/Lab/task_01_rhombus_of_stars.py
+++ 01 First Steps in OOP/Lab/task_01_rhombus_of_stars.py
@@ -1,5 +1,5 @@
 def print_rows(num, row):
-    for space in range(n - row):
+    for space in range(num - row):
         print(" ", end="")
     for star in range(1, row):
         print("*", end=' ')
@@ -7,12 +7,12 @@ def print_rows(num, row):
 
 
 def print_upper_part(num):
-    for row in range(1, n + 1):
+    for row in range(1, num + 1):
         print_rows(num, row)
 
 
 def print_lower_part(num):
-    for row in range(n - 1, 0, -1):
+    for row in range(num - 1, 0, -1):
         print_rows(num, row)
 
 
