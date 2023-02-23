35ba8a1923e82fe09682b71c12e48b336d78e9aa
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-02-23 10:26:00 +0200

    update

---
+++ 02 Classes and Objects/Exercise/task_04_pizza_delivery.py
@@ -0,0 +1,36 @@
+class PizzaDelivery:
+    def __init__(self, name: str, price: float, ingredients: dict):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
+        if ingredient not in self.ingredients:
+            self.ingredients[ingredient] = 0
+        self.ingredients[ingredient] += quantity
+        self.price += quantity * price_per_quantity
+
+    def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+        if self.ordered:
+            return f"Pizza {self.name} already prepared, and we can't make any changes!"
+
+        if ingredient not in self.ingredients:
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+
+        if self.ingredients[ingredient] - quantity < 0:
+            return f"Please check again the desired quantity of {ingredient}!"
+
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
+
+    def make_order(self):
+        self.ordered = True
+        ingredients = []
+        for ingredient, quantity in self.ingredients.items():
+            ingredients.append(f"{ingredient}: {quantity}")
+
+        return f"You've ordered pizza {self.name} prepared with {', '.join(ingredients)} and the price will be {self.price}lv."
