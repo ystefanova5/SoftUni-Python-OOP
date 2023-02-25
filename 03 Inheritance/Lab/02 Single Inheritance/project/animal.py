901ae3b047d2ad6606e06503002dbeec9dd59c12
ystefanova5 <y.stefanova@hotmail.com>
2023-02-25 11:32:05 +0200

    update

--- 03 Inheritance/Lab/02 Single Inheritance/project/animal.py
+++ 03 Inheritance/Lab/02 Single Inheritance/project/animal.py
@@ -1,6 +1,3 @@
 class Animal:
-    def __init__(self):
-        pass
-
     def eat(self):
         return "eating..."
--- 03 Inheritance/Lab/02 Single Inheritance/project/dog.py
+++ 03 Inheritance/Lab/02 Single Inheritance/project/dog.py
@@ -2,8 +2,5 @@ from project.animal import Animal
 
 
 class Dog(Animal):
-    def __init__(self):
-        super().__init__()
-
     def bark(self):
         return "barking..."
