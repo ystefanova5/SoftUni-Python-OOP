99e8d5d177b3541cf631b91e07bb5c5157feb650
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-03-03 15:05:43 +0200

    update

---
+++ 04 Encapsulation/Lab/task_04_email_validator.py
@@ -0,0 +1,19 @@
+class EmailValidator:
+    def __init__(self, min_length: int, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name: str) -> bool:
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail: str) -> bool:
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain: str) -> bool:
+        return domain in self.domains
+
+    def validate(self, email: str) -> bool:
+        name, rest = email.split("@")
+        mail, domain = rest.split(".")
+        return all([self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)])
