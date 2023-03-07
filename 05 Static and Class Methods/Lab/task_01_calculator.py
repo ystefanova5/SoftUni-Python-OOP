74ef224bdd7f2e666094e1b218720c344bcfeaf2
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-03-07 22:43:05 +0200

    update

--- 05 Static and Class Methods/Lab/task_01_calculator.py
+++ 05 Static and Class Methods/Lab/task_01_calculator.py
@@ -3,17 +3,17 @@ from functools import reduce
 
 class Calculator:
     @staticmethod
-    def add(*args):
+    def add(*args) -> int or float:
         return sum(args)
 
     @staticmethod
-    def multiply(*args):
+    def multiply(*args) -> int or float:
         return reduce(lambda a, b: a * b, args)
 
     @staticmethod
-    def divide(*args):
-        return reduce(lambda a, b: a / b, args)
+    def divide(*args) -> int or float:
+        return reduce(lambda a, b: a + b if a == 0 or b == 0 else a / b, args)
 
     @staticmethod
-    def subtract(*args):
+    def subtract(*args) -> int or float:
         return reduce(lambda a, b: a - b, args)
