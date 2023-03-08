83115f7fae9ddc17714c8346cf00da6ee28b0dea
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-03-08 14:09:29 +0200

    update

---
+++ 05 Static and Class Methods/Lab/04 Hotel Room/project/hotel.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name: str):
+        self.name = name
+        self.rooms: List[Room] = []
+        self.guests: int = 0
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room) -> None:
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people) -> str or None:
+        room = next(filter(lambda x: x.number == room_number, self.rooms))
+        room.take_room(people)
+        self.guests += room.guests
+
+    def free_room(self, room_number) -> str or None:
+        room = next(filter(lambda x: x.number == room_number, self.rooms))
+        self.guests -= self.guests
+        room.free_room()
+
+    def status(self) -> str:
+        free_rooms = [str(room.number) for room in self.rooms if not room.is_taken]
+        taken_rooms = [str(room.number) for room in self.rooms if room.is_taken]
+
+        result = [
+            f"Hotel {self.name} has {self.guests} total guests",
+            f"Free rooms: {', '.join(free_rooms)}",
+            f"Taken rooms: {', '.join(taken_rooms)}"
+        ]
+
+        return '\n'.join(result)
---
+++ 05 Static and Class Methods/Lab/04 Hotel Room/project/room.py
@@ -0,0 +1,20 @@
+class Room:
+    def __init__(self, number: int, capacity: int):
+        self.number = number
+        self.capacity = capacity
+        self.guests: int = 0
+        self.is_taken: bool = False
+
+    def take_room(self, people) -> str or None:
+        if self.is_taken or people > self.capacity:
+            return f"Room number {self.number} cannot be taken"
+
+        self.is_taken = True
+        self.guests = people
+
+    def free_room(self) -> str or None:
+        if not self.is_taken:
+            return f"Room number {self.number} is not taken"
+
+        self.is_taken = False
+        self.guests = 0
