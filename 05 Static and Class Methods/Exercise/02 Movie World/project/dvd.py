b243bd7d5c1f08b9ff0c42501c0afaf43dc493e5
ystefanova5 <110626738+ystefanova5@users.noreply.github.com>
2023-03-13 10:55:50 +0200

    update

--- 05 Static and Class Methods/Exercise/02 Movie World/project/dvd.py
+++ 05 Static and Class Methods/Exercise/02 Movie World/project/dvd.py
@@ -19,4 +19,5 @@ class DVD:
         return cls(name, dvd_id, year, months[str(month)], age_restriction)
 
     def __repr__(self):
-        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {'rented' if self.is_rented else 'not rented'}"
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) " \
+               f"has age restriction {self.age_restriction}. Status: {'rented' if self.is_rented else 'not rented'}"
