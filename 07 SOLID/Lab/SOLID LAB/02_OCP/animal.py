83e5d9532c90c66ea3ac2ff1eb3a125202a77d3c
ystefanova <y.stefanova@hotmail.com>
2023-03-16 12:58:52 +0200

    update

--- 07 SOLID/Lab/SOLID LAB/02_OCP/animal.py
+++ 07 SOLID/Lab/SOLID LAB/02_OCP/animal.py
@@ -1,22 +1,42 @@
-class Animal:
-    def __init__(self, species):
-        self.species = species
+from abc import ABC, abstractmethod
 
-    def get_species(self):
-        return self.species
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+
+class Cat(Animal):
+
+    def make_sound(self):
+        return "meow"
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return "woof-woof"
+
+
+class Chicken(Animal):
+    def make_sound(self):
+        return "pluck-pluck"
+
+
+class Cow(Animal):
+    def make_sound(self):
+        return "moo"
 
 
 def animal_sound(animals: list):
     for animal in animals:
-        if animal.species == 'cat':
-            print('meow')
-        elif animal.species == 'dog':
-            print('woof-woof')
+        print(animal.make_sound())
 
 
-animals = [Animal('cat'), Animal('dog')]
-animal_sound(animals)
+# animals = [Animal('cat'), Animal('dog')]
+# animal_sound(animals)
 
 ## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
 ## при добавяне на нови животни
-# animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
+animals = [Cat(), Dog(), Chicken(), Cow()]
+animal_sound(animals)
