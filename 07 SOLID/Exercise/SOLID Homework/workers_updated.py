613c8c6770fb43a71d98bb58a6b3ebca9d135771
ystefanova <y.stefanova@hotmail.com>
2023-03-17 17:10:47 +0200

    update

---
+++ 07 SOLID/Exercise/SOLID Homework/workers_updated.py
@@ -0,0 +1,89 @@
+from abc import ABC, abstractmethod
+import time
+
+
+class Eatable(ABC):
+    @abstractmethod
+    def eat(self):
+        ...
+
+
+class Workable(ABC):
+    @abstractmethod
+    def work(self):
+        ...
+
+
+class Worker(Eatable, Workable):
+
+    def work(self):
+        print("I'm normal worker. I'm working.")
+
+    def eat(self):
+        print("Lunch break....(5 secs)")
+        time.sleep(5)
+
+
+class SuperWorker(Eatable, Workable):
+
+    def work(self):
+        print("I'm super worker. I work very hard!")
+
+    def eat(self):
+        print("Lunch break....(3 secs)")
+        time.sleep(3)
+
+
+class Manager(ABC):
+    def __init__(self):
+        self.worker = None
+
+    @abstractmethod
+    def set_worker(self, worker):
+        ...
+
+
+class WorkManager(Manager):
+    def set_worker(self, worker):
+        assert isinstance(worker, Workable), f"`worker` must be of type {Workable}"
+
+        self.worker = worker
+
+    def manage(self):
+        self.worker.work()
+
+
+class BreakManager(Manager):
+    def set_worker(self, worker):
+        assert isinstance(worker, Eatable), f"`worker` must be of type {Eatable}"
+
+        self.worker = worker
+
+    def lunch_break(self):
+        self.worker.eat()
+
+
+class Robot(Workable):
+    def work(self):
+        print("I'm a robot. I'm working....")
+
+
+work_manager = WorkManager()
+break_manager = BreakManager()
+work_manager.set_worker(Worker())
+break_manager.set_worker(Worker())
+work_manager.manage()
+break_manager.lunch_break()
+
+work_manager.set_worker(SuperWorker())
+break_manager.set_worker(SuperWorker())
+work_manager.manage()
+break_manager.lunch_break()
+
+work_manager.set_worker(Robot())
+work_manager.manage()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
