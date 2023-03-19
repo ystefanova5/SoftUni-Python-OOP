62bb16a73fc8e218c786e433225d722649bc2f75
ystefanova5 <y.stefanova@hotmail.com>
2023-03-19 11:54:13 +0200

    update

--- 08 Iterators and Generators/Lab/task_05_generator_range.py
+++ 08 Iterators and Generators/Lab/task_05_generator_range.py
@@ -2,3 +2,7 @@ def genrange(start: int, end: int):
     while start <= end:
         yield start
         start += 1
+
+# def genrange(start: int, end: int):
+#     for num in range(start, end + 1):
+#         yield num
