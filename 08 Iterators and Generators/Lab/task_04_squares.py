cffcdfd166054b2836b0f8049918162fd2f25cb4
ystefanova5 <y.stefanova@hotmail.com>
2023-03-19 11:51:22 +0200

    update

--- 08 Iterators and Generators/Lab/task_04_squares.py
+++ 08 Iterators and Generators/Lab/task_04_squares.py
@@ -4,3 +4,22 @@ def squares(end: int):
     while num <= end:
         yield num ** 2
         num += 1
+
+# class squares:
+#     def __init__(self, end: int):
+#         self.start = 0
+#         self.end = end
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.start >= self.end:
+#             raise StopIteration
+#
+#         self.start += 1
+#
+#         return self.start ** 2
+#
+#
+# print(list(squares(5)))
