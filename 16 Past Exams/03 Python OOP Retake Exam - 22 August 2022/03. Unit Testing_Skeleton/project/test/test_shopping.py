db096b2f3ab742c3545601bbb085b1b52146b2fe
ystefanova5 <y.stefanova@hotmail.com>
2023-03-29 15:37:46 +0300

    update

--- 16 Past Exams/03 Python OOP Retake Exam - 22 August 2022/03. Unit Testing_Skeleton/project/test/test_shopping.py
+++ 16 Past Exams/03 Python OOP Retake Exam - 22 August 2022/03. Unit Testing_Skeleton/project/test/test_shopping.py
@@ -1,104 +1,107 @@
 from project.shopping_cart import ShoppingCart
-
 from unittest import TestCase, main
 
 
 class Tests(TestCase):
+    def setUp(self) -> None:
+        self.cart = ShoppingCart("Lidl", 200)
+        self.other_cart = ShoppingCart("Parkmart", 110)
+
     def test_01_init(self):
-        cart = ShoppingCart("Lidl", 50.50)
-        self.assertEqual("Lidl", cart.shop_name)
-        self.assertEqual(50.50, cart.budget)
-        self.assertEqual({}, cart.products)
+        self.assertEqual("Lidl", self.cart.shop_name)
+        self.assertEqual(200, self.cart.budget)
+        self.assertEqual({}, self.cart.products)
 
     def test_02_invalid_name_raises(self):
+        expected_message = "Shop must contain only letters and must start with capital letter!"
         with self.assertRaises(ValueError) as ex:
-            ShoppingCart("lidl1", 100)
-        self.assertEqual("Shop must contain only letters and must start with capital letter!", str(ex.exception))
+            self.cart.shop_name = "Lidl1"
 
-    def test_03_add_with_price_equal_to_100_raises(self):
-        cart = ShoppingCart("Lidl", 200)
-
-        with self.assertRaises(ValueError) as ex:
-            cart.add_to_cart("Cheese", 100)
-
-        self.assertEqual("Product Cheese cost too much!", str(ex.exception))
-        self.assertEqual({}, cart.products)
-
-    def test_04_add_with_price_over_100_raises(self):
-        cart = ShoppingCart("Lidl", 200)
+        self.assertEqual(expected_message, str(ex.exception))
 
         with self.assertRaises(ValueError) as ex:
-            cart.add_to_cart("Cheese", 100.01)
+            self.cart.shop_name = "lidl"
 
-        self.assertEqual("Product Cheese cost too much!", str(ex.exception))
-        self.assertEqual({}, cart.products)
+        self.assertEqual(expected_message, str(ex.exception))
 
-    def test_05_add_to_cart_valid_price(self):
-        cart = ShoppingCart("Lidl", 100)
+    def test_03_add_to_cart_over_100_raises(self):
+        expected_message = "Product Cheese cost too much!"
+        with self.assertRaises(ValueError) as ex:
+            self.cart.add_to_cart("Cheese", 100)
 
-        cart.add_to_cart("Cheese", 10)
-        self.assertEqual({"Cheese": 10}, cart.products)
+        self.assertEqual(expected_message, str(ex.exception))
+        self.assertEqual({}, self.cart.products)
 
-        result = cart.add_to_cart("Bread", 2)
-        self.assertEqual({"Cheese": 10, "Bread": 2}, cart.products)
-        self.assertEqual("Bread product was successfully added to the cart!", result)
+        expected_message = "Product Ham cost too much!"
+        with self.assertRaises(ValueError) as ex:
+            self.cart.add_to_cart("Ham", 101)
 
-        cart.add_to_cart("Bread", 2.50)
-        self.assertEqual({"Cheese": 10, "Bread": 2.50}, cart.products)
+        self.assertEqual(expected_message, str(ex.exception))
+        self.assertEqual({}, self.cart.products)
 
-    def test_06_remove_invalid_product_raises(self):
-        cart = ShoppingCart("Lidl", 200)
-        cart.add_to_cart("Cheese", 10)
+    def test_04_add_to_cart_success(self):
+        expected_message = "Cheese product was successfully added to the cart!"
+        result = self.cart.add_to_cart("Cheese", 10)
+        self.cart.add_to_cart("Ham", 12)
+        self.cart.add_to_cart("Ham", 15)
+
+        self.assertEqual(expected_message, result)
+        self.assertEqual({"Cheese": 10, "Ham": 15}, self.cart.products)
+
+    def test_05_remove_from_cart_invalid_product_raises(self):
+        expected_message = "No product with name Ham in the cart!"
+        self.cart.add_to_cart("Cheese", 10)
 
         with self.assertRaises(ValueError) as ex:
-            cart.remove_from_cart("Ham")
+            self.cart.remove_from_cart("Ham")
 
-        self.assertEqual("No product with name Ham in the cart!", str(ex.exception))
+        self.assertEqual(expected_message, str(ex.exception))
+        self.assertEqual({"Cheese": 10}, self.cart.products)
 
-    def test_07_remove_valid_product(self):
-        cart = ShoppingCart("Lidl", 200)
-        cart.add_to_cart("Cheese", 10)
-        cart.add_to_cart("Ham", 8)
-        self.assertEqual({"Cheese": 10, "Ham": 8}, cart.products)
+    def test_06_remove_from_cart_success(self):
+        self.cart.add_to_cart("Cheese", 10)
+        self.cart.add_to_cart("Ham", 12)
 
-        result = cart.remove_from_cart("Ham")
+        result = self.cart.remove_from_cart("Cheese")
+        expected_result = "Product Cheese was successfully removed from the cart!"
 
-        self.assertEqual("Product Ham was successfully removed from the cart!", result)
-        self.assertEqual({"Cheese": 10}, cart.products)
+        self.assertEqual(expected_result, result)
+        self.assertEqual({"Ham": 12}, self.cart.products)
 
-    def test_08_add_two_shops(self):
-        cart = ShoppingCart("Lidl", 150)
-        cart.add_to_cart("Cheese", 10)
-        cart.add_to_cart("Ham", 8)
+    def test_07_add_method(self):
+        self.cart.add_to_cart("Cheese", 10)
+        self.other_cart.add_to_cart("Bread", 2)
+        self.other_cart.add_to_cart("Ham", 15)
 
-        other_cart = ShoppingCart("Parkmart", 200)
-        other_cart.add_to_cart("Cheese", 10)
-        other_cart.add_to_cart("Bread", 3)
+        merged_cart = self.cart + self.other_cart
 
-        new_cart = cart + other_cart
-        self.assertEqual("LidlParkmart", new_cart.shop_name)
-        self.assertEqual(350, new_cart.budget)
-        self.assertEqual({'Cheese': 10, 'Ham': 8, 'Bread': 3}, new_cart.products)
+        self.assertEqual("LidlParkmart", merged_cart.shop_name)
+        self.assertEqual(310, merged_cart.budget)
+        self.assertEqual({"Cheese": 10, "Bread": 2, "Ham": 15}, merged_cart.products)
 
-    def test_09_buy_product_over_budget_raises(self):
-        cart = ShoppingCart("Lidl", 15)
-        cart.add_to_cart("Cheese", 10.20)
-        cart.add_to_cart("Ham", 8)
+    def test_08_buy_product_insufficient_budget_raises(self):
+        expected_result = "Not enough money to buy the products! Over budget with 4.50lv!"
+        self.cart.budget = 20.50
+        self.cart.add_to_cart("Cheese", 10)
+        self.cart.add_to_cart("Ham", 15)
 
         with self.assertRaises(ValueError) as ex:
-            cart.buy_products()
+            self.cart.buy_products()
 
-        self.assertEqual("Not enough money to buy the products! Over budget with 3.20lv!", str(ex.exception))
+        self.assertEqual(expected_result, str(ex.exception))
+        self.assertEqual(20.50, self.cart.budget)
 
-    def test_10_buy_product_success(self):
-        cart = ShoppingCart("Lidl", 100)
-        cart.add_to_cart("Cheese", 10.20)
-        cart.add_to_cart("Ham", 8)
+    def test_09_buy_products_success(self):
+        expected_result = "Products were successfully bought! Total cost: 25.00lv."
+        self.cart.add_to_cart("Cheese", 10)
+        self.cart.add_to_cart("Ham", 15)
 
-        result = cart.buy_products()
-        self.assertEqual("Products were successfully bought! Total cost: 18.20lv.", result)
-        self.assertEqual({'Cheese': 10.20, 'Ham': 8}, cart.products)
-        self.assertEqual(100, cart.budget)
+        result = self.cart.buy_products()
+        self.assertEqual(expected_result, result)
+
+        self.cart.budget = 25
+        result = self.cart.buy_products()
+        self.assertEqual(expected_result, result)
 
 
 if __name__ == "__main__":
